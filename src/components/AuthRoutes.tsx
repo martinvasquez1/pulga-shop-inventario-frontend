23670937bca3b41592ac7dcbec0724d50a1ee265
Martin Vasquez <159503418+martinvasquez1@users.noreply.github.com>
2025-10-18 17:06:06 -0300

    Add AppLayout

--- src/components/AuthRoutes.tsx
+++ src/components/AuthRoutes.tsx
@@ -1,11 +1,12 @@
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate } from "react-router-dom";
+import AppLayout from "../layouts/app/AppLayout";
 
-export default function AuthRoutes({}) {
+export default function AuthRoutes() {
   const isAuthenticated = Boolean(localStorage.getItem("jwt"));
 
   if (!isAuthenticated) {
     return <Navigate to="/auth/login" />;
   }
 
-  return <Outlet />;
+  return <AppLayout />;
 }
