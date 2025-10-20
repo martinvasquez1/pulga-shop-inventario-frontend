6f2a5d7c4b43869c6ab3828a5c521607322af5ac
Martin Vasquez <159503418+martinvasquez1@users.noreply.github.com>
2025-10-20 15:57:02 -0300

    Add shop modal

--- src/views/app/appHome.tsx
+++ src/views/app/appHome.tsx
@@ -1,16 +1,30 @@
-import { Button } from "@mui/material";
+import { useState } from "react";
 
-import ShopList from "../../components/shop/shopList";
+import { Button } from "@mui/material";
 import { Create } from "@mui/icons-material";
 
+import ShopList from "../../components/shop/shopList";
+import CreateShopModal from "../../components/shop/create-shop-modal";
+
 export default function AppHome({}) {
+  const [isFormOpen, setIsFormOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setIsFormOpen(true);
+  };
+
   return (
     <div className="w-full">
       <div className="flex justify-between items-center gap-8">
         <h1 className="text-3xl font-bold">Tiendas</h1>
-        <Button variant="contained" endIcon={<Create />}>
+        <Button
+          variant="contained"
+          endIcon={<Create />}
+          onClick={handleClickOpen}
+        >
           Create
         </Button>
+        <CreateShopModal open={isFormOpen} setOpen={setIsFormOpen} />
       </div>
       <ShopList />
     </div>
