16a050ddac9f083d5aecea3f88e3e994bf60d8d5
Martin Vasquez <159503418+martinvasquez1@users.noreply.github.com>
2025-10-23 16:25:15 -0300

    Update store card to use random image

--- src/components/shop/storeGridItem.tsx
+++ src/components/shop/storeGridItem.tsx
@@ -6,20 +6,41 @@ import { Button } from "@mui/material";
 
 import { CreateShopResponse } from "../../api/shop/createShop";
 import { StyledCard, StyledCardContent, StyledTypography } from "../Card";
-import Picture from "../../assets/images/card.jpg";
+
+import picture1 from "../../assets/images/store_images/blue.jpg";
+import picture2 from "../../assets/images/store_images/blue2.jpg";
+import picture3 from "../../assets/images/store_images/dark_blue.jpg";
+import picture4 from "../../assets/images/store_images/light_pink.jpg";
+import picture5 from "../../assets/images/store_images/orange.jpg";
+import picture6 from "../../assets/images/store_images/pink.jpg";
+
+const images = [picture1, picture2, picture3, picture4, picture5, picture6];
+
+function getRandomPicture(name: string) {
+  const normalizedName = name.toLowerCase().replace(/[^a-z]/g, "");
+
+  let hash = 0;
+  for (let i = 0; i < normalizedName.length; i++) {
+    hash += normalizedName.charCodeAt(i);
+  }
+
+  return images[hash % images.length];
+}
 
 interface StoreGridItem {
   data: CreateShopResponse;
 }
 
 export function StoreGridItem({ data }: StoreGridItem) {
+  const randomPicture = getRandomPicture(data.nombre);
+
   return (
     <Grid2 size={{ xs: 12, md: 6 }}>
       <StyledCard variant="outlined" tabIndex={0}>
         <CardMedia
           component="img"
           alt="green iguana"
-          image={Picture}
+          image={randomPicture}
           sx={{
             aspectRatio: "16 / 9",
             borderBottom: "1px solid",
