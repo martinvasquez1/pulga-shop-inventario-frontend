258c4914de5a5a71d70d6847a19b3918bbaaf978
Martin Vasquez <159503418+martinvasquez1@users.noreply.github.com>
2025-11-09 16:13:55 -0300

    Update product drawer to display info

--- src/components/product/product-drawer.tsx
+++ src/components/product/product-drawer.tsx
@@ -2,16 +2,29 @@ import {
   Box,
   List,
   ListItem,
-  ListItemButton,
-  ListItemIcon,
   ListItemText,
   Divider,
   Drawer,
+  Typography,
 } from "@mui/material";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
 import MailIcon from "@mui/icons-material/Mail";
 import { Product } from "../../types/api";
 
+interface DrawerItemProps {
+  name: string;
+  text: string;
+  icon: React.ReactNode;
+}
+
+function DrawerItem({ name, text, icon }: DrawerItemProps) {
+  return (
+    <ListItem disablePadding>
+      {icon}
+      <ListItemText primary={name} />
+      <p>{text}</p>
+    </ListItem>
+  );
+}
 interface ProductDrawerProps {
   open: boolean;
   toggleDrawer: (v: boolean) => void;
@@ -28,36 +41,78 @@ export default function ProductDrawer({
   return (
     <Drawer open={open} onClose={() => toggleDrawer(false)}>
       <Box
-        sx={{ width: 250 }}
+        sx={{ width: 500, padding: 2 }}
         role="presentation"
         onClick={() => toggleDrawer(false)}
       >
-        {product.nombre}
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            gap: 4,
+          }}
+        >
+          <div>
+            <Typography variant="h6" gutterBottom>
+              {product.nombre}
+            </Typography>
+            <Typography>SKU: {product.sku}</Typography>
+          </div>
+        </Box>
+
         <List>
-          {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItemButton>
-            </ListItem>
-          ))}
+          <DrawerItem
+            name="Descripción"
+            text={product.descripcion}
+            icon={<MailIcon />}
+          />
+          <DrawerItem name="Marca" text={product.marca} icon={<MailIcon />} />
+          <DrawerItem
+            name="Precio"
+            text={String(product.precio)}
+            icon={<MailIcon />}
+          />
+          <DrawerItem
+            name="Condición"
+            text={product.condicion}
+            icon={<MailIcon />}
+          />
+          <DrawerItem
+            name="Stock"
+            text={String(product.stock)}
+            icon={<MailIcon />}
+          />
+
+          <DrawerItem name="Categorias" text="" icon={<MailIcon />} />
+          <div>
+            {product.categorias.map((c) => (
+              <p>{c}</p>
+            ))}
+          </div>
+
+          <DrawerItem
+            name="Rating"
+            text={String(product.rating)}
+            icon={<MailIcon />}
+          />
+          <DrawerItem
+            name="Disponible"
+            text={String(product.disponible)}
+            icon={<MailIcon />}
+          />
         </List>
         <Divider />
-        <List>
-          {["All mail", "Trash", "Spam"].map((text, index) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
+        <p>Fotos</p>
+        <div>
+          {product.fotos.map((f) => {
+            return (
+              <div className="aspect-square">
+                <img src={f} />
+              </div>
+            );
+          })}
+        </div>
       </Box>
     </Drawer>
   );
